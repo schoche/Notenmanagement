348687ee4aa1bc82dcda499269b5c9966c03b6db
Elias <elias.wollitzer@htl-braunau.at>
2018-02-26 11:04:10 +0100

    npm

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express=require('express')
-
 const app=express()
 
+const bodyParser=require('body-parser')
+const mysql=require("mysql")
+
+
 app.use(express.static('public'))
+app.use(bodyParser.json())
+
+let connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "Notenmanagement"
+
+})
+
+app.get("/test",function(req,res){
+    connection.query("select * from test",function(err,results,fields){
+        console.log(results)
+    })
+})
 
 
 app.listen(3000,function(){
