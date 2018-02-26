7d5d5f2e6307be9a20e3da6607b0e0566c64adb6
MS <martin.schachl@htl-braunau.attouch index.htmltouch index.htmllsmore index.htmlgit config --global user.email martin.schachl@htl-braunau.at>
2018-02-26 12:35:23 +0100

    html1

---
+++ public/index.js
@@ -0,0 +1,28 @@
+console.log("index script loaded")
+
+let ausgabe = document.getElementById('newestevents')
+ausgabe.innerHTML="<p>das ist ein test</p>"
+
+let httpReq=new XMLHttpRequest()
+httpReq.open('GET', '/api/newest_events')
+httpReq.onload = function() {
+    let responseData = JSON.parse(this.responseText)
+    generateDataTable(responseData)
+}
+httpReq.send()
+
+
+
+function generateDataTable(data) {
+    let divEl = document.getElementById('newestevents')
+    let html = '<table>'
+    html += '<tr><th>Wort</th><th>Beschreibung</th></tr>'
+    for(let word in data) {
+        html += '<tr>'
+        html += '<td>'+word+'</td>'
+        html += '<td>'+data[word]+'</td>'
+        html += '</tr>'
+    }
+    html += '</table>'
+    divEl.innerHTML = html
+}
