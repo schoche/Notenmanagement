fdd5d2c31095f1f2d3eabb9c489fa3860153590e
Elias <elias.wollitzer@htl-braunau.at>
2018-02-26 12:22:42 +0100

    getnewest

--- server.js
+++ server.js
@@ -22,6 +22,18 @@ app.get("/test",function(req,res){
     })
 })
 
+app.get("/api/newest_events",function(req,res){
+    let query = "select klassen.klasse,faecher.fach,tests.datum from klassen join faecher join tests on klassen.kid = tests.kid AND faecher.fid = tests.fid order by datum desc"
+    connection.query(query,function(err,results,fields){
+        if(err){
+            console.log(err)
+            return
+        }
+        console.log(results[0])
+        console.log(fields)
+    })
+})
+
 
 app.listen(3000,function(){
     console.log('server running and listening on port 3000')
