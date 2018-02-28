3517091c9a186097efafad891b9e06d644ba87b9
MS <martin.schachl@htl-braunau.attouch index.htmltouch index.htmllsmore index.htmlgit config --global user.email martin.schachl@htl-braunau.at>
2018-02-28 16:00:08 +0100

    28.2.2018

--- public/index.js
+++ public/index.js
@@ -6,23 +6,164 @@ ausgabe.innerHTML="<p>das ist ein test</p>"
 let httpReq=new XMLHttpRequest()
 httpReq.open('GET', '/api/newest_events')
 httpReq.onload = function() {
-    let responseData = JSON.parse(this.responseText)
-    generateDataTable(responseData)
+let responseData = JSON.parse(this.responseText)
+generateDataTable(responseData)
+console.log(responseData)
 }
 httpReq.send()
 
-
-
 function generateDataTable(data) {
     let divEl = document.getElementById('newestevents')
-    let html = '<table>'
-    html += '<tr><th>Klasse</th><th>Fach</th><th>Datum</th></th><th>Bezeichnung</th></tr>'
-    for(let word in data) {
+    console.log(data[0].fach)
+    let html = '<table class="newest">'
+    html += '<tr><th>Klasse</th><th>Fach</th><th>Datum</th></th></tr>'
+    for(let row in data) {
         html += '<tr>'
-        html += '<td>'+word+'</td>'
-        html += '<td>'+data[word]+'</td>'
+        html += '<td  onclick="row('+data[row].tid+')">'+data[row].klasse+'</a></td>'
+        html += '<td  onclick="row('+data[row].tid+')">'+data[row].fach+'</a></td>'
+        html += '<td  onclick="row('+data[row].tid+')">'+data[row].datum+'</a></td>'
         html += '</tr>'
     }
     html += '</table>'
     divEl.innerHTML = html
 }
+
+function row(tid){
+    console.log(tid)
+    let httpReq=new XMLHttpRequest()
+    httpReq.open('GET', '/api/get_test/'+tid)
+    httpReq.onload = function() {
+    let ausgabe = document.getElementById('newestevents')
+        if(this.status==200) {
+            let responseData = JSON.parse(this.responseText)
+            console.log(responseData)
+
+            html ='<h1>'+responseData[0].klasse+': '+responseData[0].fach+' vom '+responseData[0].datum+' über '+responseData[0].bezeichnung+'</h2>'
+            html += '<table class="newest">'
+            html += '<tr><th>Vorname</th><th>Nachname</th><th>Note</th><th>Kommentar</th></tr>'
+            for(let row in responseData) {
+                html += '<tr>'
+                html += '<td>'+responseData[row].firstname+'</td>'
+                html += '<td>'+responseData[row].lastname+'</td>'
+                html += '<td>'+responseData[row].note+'</td>'
+                html += '<td>'+responseData[row].kommentar+'</td>'
+                html += '</tr>'
+            }
+            html += '</table>'
+
+
+            ausgabe.innerHTML = html
+
+
+
+
+        } else {
+            console.log("error")
+            let errorTxt = 'Error: '+this.status+' ('+this.statusText+')'
+            ausgabe.innerHTML=errorTxt
+            
+        }
+    }
+    httpReq.onerror = function(error) {
+        console.log('*** onerror ***')
+        console.log(error)
+    }
+    httpReq.send()
+}
+
+function findeklasse(){
+    let httpReq=new XMLHttpRequest()
+    httpReq.open('GET', '/api/get_klassen')
+    httpReq.onload = function() {
+    let ausgabe = document.getElementById('newestevents')
+        if(this.status==200) {
+            let responseData = JSON.parse(this.responseText)
+            console.log(responseData)
+
+
+            let html = '<table class="newest">'
+            html += '<tr><th>Klassen</th></tr>'
+            for(let row in responseData) {
+            html += '<tr>'
+            html += '<td  onclick="get_klassen('+responseData[row].kid+')">'+responseData[row].klasse+'</a></td>'
+            html += '</tr>'
+    }
+    html += '</table>'
+    ausgabe.innerHTML=html
+        } else {
+            console.log("error")
+            let errorTxt = 'Error: '+this.status+' ('+this.statusText+')'
+            ausgabe.innerHTML=errorTxt 
+        }
+    }
+    httpReq.onerror = function(error) {
+        console.log('*** onerror ***')
+        console.log(error)
+    }
+    httpReq.send()
+}
+
+function get_klassen(kid){
+    console.log(kid)
+}
+
+
+function eingabe(){
+    console.log("input")
+    let httpReq=new XMLHttpRequest()
+    httpReq.open('GET', '/api/get_klassen')
+    httpReq.onload = function() {
+        let ausgabe = document.getElementById('newestevents')
+        if(this.status==200) {
+            let responseData = JSON.parse(this.responseText)
+            console.log(responseData)
+
+/*
+            var html ='<form onsubmit="submitpressed(this);return false">'
+            html +='<select name="Klasse:" size="10">'
+            for(let row in responseData) {
+                html +='<option value="'+responseData[row].kid+'">'+responseData[row].klasse+'</option>'
+            }
+            html+='</select>'
+            html+= '<form onsubmit="submit(this);return false">'
+            html+='<input type="submit" value="Speichern">'
+            html+='</form>'
+            ausgabe.innerHTML=html
+*/
+
+        } else {
+            console.log("error")
+            let errorTxt = 'Error: '+this.status+' ('+this.statusText+')'
+            ausgabe.innerHTML=errorTxt
+            
+        }
+    }
+    httpReq.send()
+}
+
+/*
+function submit();return false
+{
+    console.log('jo')
+}
+*/
+function findeschueler(){
+    console.log('schueler')
+    ausgabe =document.getElementById('newestevents')
+    html='<p>Nachname Vorname</p>'
+    html+= '<form onsubmit="submitpressed(this);return false">'
+    html+='<input type="text" name="inputname" placeholder="Name"<br>'
+    html+='<input type="submit" value="Speichern">'
+    html+='</form>'
+    ausgabe.innerHTML=html
+}
+/*
+function submitpressed(formEl);return false
+{
+    console.log('fein')
+    console.log(formEl.elements.isn.value)
+}*/
+
+
+    
+
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express=require('express')
 const app=express()
+const path = require("path")
 
 const bodyParser=require('body-parser')
 const mysql=require("mysql")
@@ -19,35 +20,35 @@ let connection = mysql.createConnection({
 
 connection.connect(function(err){
     if(err){
-        console.log(err)
+        console.log("SQL Connection error:" + err)
         return
     }
     console.log("connected to DB")
 })
 
-app.get("/test",function(req,res){
-    connection.query("select * from tests",function(err,results,fields){
-        console.log(results)
-    })
-})
+app.get('/homepage', function(req, res) {
+    console.log("homepage query")
+    res.sendFile(path.join(__dirname + '/public/index.html'))
+    //res.send("homepage")
+});
 
 app.get("/api/newest_events",function(req,res){
-    let query = 'select klassen.klasse,faecher.fach,date_format(tests.datum,"%d-%m-%Y") as datum from klassen join faecher join tests on klassen.kid = tests.kid AND faecher.fid = tests.fid'
+    let query = 'select klassen.klasse,faecher.fach,tests.tid,date_format(tests.datum,"%d-%m-%Y") as datum from klassen join faecher join tests on klassen.kid = tests.kid AND faecher.fid = tests.fid'
     connection.query(query,function(err,results,fields){
         if(err){
-            console.log(err)
+            console.log("get newest_events ERROR: " + err)
             return
         }
         let x = JSON.stringify(results)
         let y = JSON.parse(x)
-        console.log(y)
+        console.log("newest_events:\n" + y)
 
         let len = getNumber(x)
         console.log(len)
 
         let five = []
         if(len > 5){
-            for(let i = 0; i < 5; i++){
+            for(let i = len-5; i < len; i++){
                 five.push(y[i])
             }
         }else{
@@ -57,8 +58,28 @@ app.get("/api/newest_events",function(req,res){
     })
 })
 
+app.get("/api/get_test/:tid",function(req,res){
+    let query = 'select s.sid, s.firstname, s.lastname, k.kid, k.klasse, e.note, e.kommentar, t.tid, t.bezeichnung, date_format(t.datum,"%d-%m-%Y") as datum, f.fid, f.fach ' + 
+                'from schueler as s join ergebnisse as e join tests as t join klassen as k join faecher as f ' +
+                'on e.sid = s.sid and e.tid = t.tid and s.kid =  t.kid ' +
+                'and k.kid = s.kid and f.fid = t.fid where t.tid = ?'; 
+
+    console.log("tid: " + req.params.tid)
+    connection.query(query,req.params.tid,function(err,results,fields){
+        if(err){
+            console.log("get test ERROR: " + err)
+            return
+        }
+        let x = JSON.stringify(results)
+        let y = JSON.parse(x)
+        console.log(y)
+
+        res.send(y)
+    })
+})
+
 app.get("/api/get_klassen",function(req,res){
-    let query = 'select klasse from klassen'
+    let query = 'select * from klassen'
     connection.query(query,function(err,results,fields){
         if(err){
             console.log(err)
