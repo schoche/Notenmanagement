2804bb869e2cbd16fa3eab7ba2fca7e065dcb894
jovanjo13 <elias.wollitzer@htl-braunau.at>
2018-03-19 08:17:52 +0100

    tests

--- server.js
+++ server.js
@@ -83,11 +83,32 @@ app.get("/api/get_test/:tid",function(req, res){
 })
 
 app.post("/api/post_test", function(req, res){
-    kid = req.body.kid
-    lid = req.body.lid
-    fid = req.body.fid
-    bez = req.body.bezeichnung
-    dat = reverseDate(req.body.datum)
+    let tid = req.body.tid
+    console.log(tid)
+    if(tid == 0){
+        post_test(req, function(tid){
+            res.send(tid)
+        })
+    }else{
+        delete_test(tid, function(){
+            post_test(req, function(tid){
+                res.send(tid)
+            })
+        })
+
+    }
+
+    
+})
+
+function post_test(req, callback){
+    
+    let kid = req.body.kid
+    let lid = req.body.lid
+    let fid = req.body.fid
+    let bez = req.body.bezeichnung
+    let dat = reverseDate(req.body.datum)
+    let tid = req.body.tid
 
     let query = 'insert into tests values (null,' + kid + ',' + lid +',' + fid + ',"' + bez + '","' + dat + '")'
     console.log("post_test:\n" + query)
@@ -105,36 +126,19 @@ app.post("/api/post_test", function(req, res){
 
         get_tid(kid, lid, fid, bez, dat, function(tid){
             console.log("get_tid: " + JSON.stringify(tid))
-            res.send(tid)
+            callback(tid)
         })
     })
-})
-
-app.post("/api/post_ergebnisse", function(req, res){
-    console.log(req.body)
-    res.send(req.body)
-})
+}
 
 app.post("/api/delete_test", function(req, res){
     tid = req.body.tid
-    console.log("delete_test tid: " + tid)
-
-    let query = 'delete from tests where tests.tid = ' + tid +
-                '; delete from ergebnisse where ergebnisse.tid = ' + tid
     
-    connection.query(query, function(err, results, fields){
-        if(err){
-            console.log("post delete_test ERROR: " + err)
-            return
-        }
+    delete_test(tid)
 
-        let x = JSON.stringify(results)
-        let y = JSON.parse(x)
-        console.log(y)
-
-        
-    })
+    
 })
+
 
 app.get("/api/get_klassen_faecher/:kid",function(req, res){
     let query = 'select distinct * from (select f.fid, f.fach, k.kid, k.klasse from tests as t join faecher as f join klassen as k ' +
@@ -256,6 +260,27 @@ app.listen(3000,function(){
     console.log('server running and listening on port 3000')
 })
 
+function delete_test(tid, callback){
+    
+    console.log("delete_test tid: " + tid)
+    let query = 'delete from tests where tests.tid = ' + tid +
+                '; delete from ergebnisse where ergebnisse.tid = ' + tid
+    
+    connection.query(query, function(err, results, fields){
+        if(err){
+            console.log("delete_test ERROR: " + err)
+            return
+        }
+
+        let x = JSON.stringify(results)
+        let y = JSON.parse(x)
+        console.log("deleted")
+        callback()
+
+        
+    })
+}
+
 function get_tid(kid, lid, fid, bez, revDat, callback){
     let query = 'select t.tid from tests as t where t.kid = ' + kid + ' and t.lid = ' + lid + ' and t.fid = ' + fid + 
                 ' and t.bezeichnung = "' + bez + '" and t.datum = "' + revDat + '"';
@@ -280,7 +305,7 @@ function get_schuelertests(fn, ln, callback){
                 'and t.lid = l.lid ' + 
                 'and t.fid = f.fid ' +
                 'and s.kid = k.kid ' +
-                'where s.firstname = "' + fn + '" and s.lastname = "' + ln + '"';
+                'where s.firstname = "' + fn + '" and s.lastname = "' + ln + '" order by f.fid';
     
     console.log(query)
     connection.query(query,function(err, results, fields){
@@ -289,12 +314,41 @@ function get_schuelertests(fn, ln, callback){
             return
         }
         let x = JSON.stringify(results)
-        let y = JSON.parse(x)
-        console.log("get_schuelertests\n" + y)
+        
+        let y = formfacharray(x)
+        console.log("get_schuelertests\n")
+        console.log(y)
         
         callback(y)
     })
 
+}
+
+function formfacharray(str){
+    let obj = JSON.parse(str)
+    let len = getNumber(str)
+    if(!len){
+        return obj
+    }
+    let fach = obj[0].fach
+    let resObj = []
+    resObj.push({"fach":fach, "data": []})
+    let ind = 0
+    
+    for(let i in obj){
+        if(obj[i].fach != fach){
+            fach = obj[i].fach
+            ind++
+            resObj.push({"fach":fach, "data": []})
+            console.log(fach)
+            
+        }
+        resObj[ind]["data"].push(obj[i])
+
+        
+    }
+    console.log(resObj)
+    return resObj
 }
 
 function get_lehrertests(fn, ln, callback){
